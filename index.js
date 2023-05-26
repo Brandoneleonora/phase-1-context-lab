640114a17dca151223d92ea5a3a0eaa1c876b10d
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-05-26 19:45:53 -0400

    complete project

--- index.js
+++ index.js
@@ -21,3 +21,95 @@ const allWagesFor = function () {
     return payable
 }
 
+const createEmployeeRecord = (str) => {
+   const card = {
+    firstName:NaN,
+    familyName:NaN,
+    title:NaN,
+    payPerHour:NaN,
+    timeInEvents:NaN,
+    timeOutEvents:NaN
+   }
+
+   card.firstName = str[0]
+   card.familyName = str[1]
+   card.title = str[2]
+   card.payPerHour = str[3]
+   card.timeInEvents = []
+   card.timeOutEvents = []
+
+   return card
+}
+
+
+const createEmployeeRecords = (arr) => {
+    //creates an employee card for each array element 
+    const employeeCards = arr.map(ar => createEmployeeRecord(ar))
+    //console.log(the cards in a new array)
+    //console.log(employeeCards)
+    return employeeCards
+}
+
+  
+function createTimeInEvent(arg){
+    //Need to create a new object
+    const timeIn = {
+        type:"TimeIn",
+        hour:parseInt(arg.substr(11, 15)),
+        date:arg.substr(0, 10)
+    }  
+
+    this.timeInEvents.push(timeIn)
+
+
+    return this
+}
+
+function createTimeOutEvent(arg){
+    //Need to create a new object
+    const timeOut = {
+        type:"TimeOut",
+        hour:parseInt(arg.substr(11, 15)),
+        date:arg.substr(0, 10)
+    }  
+
+    this.timeOutEvents.push(timeOut)
+
+
+    return this
+}
+
+function hoursWorkedOnDate(arg){
+    const inHour = this.timeInEvents.find(hour => hour.date === arg)
+    const inOut = this.timeOutEvents.find(hour => hour.date === arg)
+    let result = (inOut.hour - inHour.hour) / 100
+
+    return result
+}
+
+function wagesEarnedOnDate(date){
+    let pay = this.payPerHour
+    let hour = hoursWorkedOnDate.call(this, date)
+    
+    return pay * hour
+}
+
+
+
+function findEmployeeByFirstName(cards, name){
+    const card = cards.find(first => first.firstName === name)
+
+    return card
+}
+
+function calculatePayroll(arr){
+    let allWages = []
+    arr.forEach(ar => allWages.push(allWagesFor.call(ar)))
+
+    const total = allWages.reduce((total, current) => total + current, 0)
+
+    console.log(total)
+
+    return total
+
+}
